ea4f06b1b8a47a50676a1b5db76366558996b5e2
Lubel29 <137845691+Lubel29@users.noreply.github.com>
2024-03-14 21:07:45 -0300

    Delete dcl_gestion.sql

--- dcl_gestion.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER "prueba"@"localhost" identified by "123456";
-
-GRANT SELECT on datos_paciente to "prueba"@"localhost";
-GRANT UPDATE,INSERT ON gestion_hospitalaria.consulta TO 'prueba'@'localhost';
-
-SHOW GRANTS FOR "prueba"@"localhost";
